90a7234b07661e8b183b5bb8e179ce5e750143ca
Aruzhan Muratbek <aruzhan_1106@mail.ru>
2024-02-16 13:47:37 +0600

    small fixes

--- greenapi-test/src/App.jsx
+++ greenapi-test/src/App.jsx
@@ -1,33 +1,136 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [idInstance, setIdInstance] = useState('');
+  const [apiTokenInstance, setApiTokenInstance] = useState('');
+  const [phoneNumberMessage, setPhoneNumberMessage] = useState('');
+  const [phoneNumberFile, setPhoneNumberFile] = useState('');
+  const [message, setMessage] = useState('');
+  const [fileUrl, setFileUrl] = useState('');
+  const [responseData, setResponseData] = useState('');
+
+  const handleGetSettings = async() => {
+    try {
+      const response = await fetch(`https://api.green-api.com/waInstance${idInstance}/getSettings/${apiTokenInstance}`, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+        },
+      });
+      
+      const result = await response.json();
+      setResponseData(JSON.stringify(result, null, 4));
+    } catch (err) {
+      setErr(err.message); }
+  }
+
+  const handleGetStateInstance = async() => {
+    try {
+      const response = await fetch(`https://api.green-api.com/waInstance${idInstance}/getStateInstance/${apiTokenInstance}`, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+        },
+      });
+
+      const result = await response.json();
+      setResponseData(JSON.stringify(result, null, 4));
+    } catch (err) {
+      setErr(err.message); 
+    }
+  }
+
+  const handleSendMessage = async() => {
+    try {
+      const response = await fetch(`https://api.green-api.com/waInstance${idInstance}/sendMessage/${apiTokenInstance}`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+        },
+        body: JSON.stringify({chatId: phoneNumberMessage + "@c.us", message: message})
+      });
+      const result = await response.json();
+      console.log(JSON.stringify(result, null, 4));
+      setResponseData(JSON.stringify(result, null, 4));
+    } catch (err) {
+      setErr(err.message); 
+    }
+  }
+
+  const handleSendFile = async() => { 
+    const fileName = fileUrl.substring(fileUrl.lastIndexOf('/') + 1, fileUrl.length);
+    setFileUrl(fileUrl.substring(0, fileUrl.lastIndexOf('/')));
+    try {
+      const response = await fetch(`https://api.green-api.com/waInstance${idInstance}/sendFileByUrl/${apiTokenInstance}`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+        },
+        body: JSON.stringify({chatId: phoneNumberFile + "@c.us", urlFile: fileUrl, fileName: fileName})
+      });
+      const result = await response.json();
+      console.log(JSON.stringify(result, null, 4));
+      setResponseData(JSON.stringify(result, null, 4));
+    } catch (err) {
+      setErr(err.message); 
+    }
+   }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="mainField">
+
+      <div className="inputFields">
+
+        <div className='instanceInputs'>
+          <input type="text" name="idInstance" placeholder="idInstance" 
+                  value={idInstance} 
+                  onChange={e => setIdInstance(e.target.value)} />
+          <input type="text" name="ApiTokenInstance" placeholder="ApiTokenInstance"
+                  value={apiTokenInstance} 
+                  onChange={e => setApiTokenInstance(e.target.value)} />
+        </div>
+
+        <div className='getButtons'>
+          <button type="button" onClick={handleGetSettings}>getSettings</button>
+          <button type="button" onClick={handleGetStateInstance}>getStateInstance</button>
+        </div>
+
+        <div className='sendMessage'>
+          <input type="text" name="phoneNumberMessage" placeholder="phone number" 
+            value={phoneNumberMessage} 
+            onChange={e => setPhoneNumberMessage(e.target.value)}/>
+
+          <input type="text" name="message" placeholder="message"
+          value={message} 
+          onChange={e => setMessage(e.target.value)}/>
+
+          <button type="button" onClick={handleSendMessage}>sendMessage</button>
+        </div>
+
+        <div className='sendFile'>
+          <input type="text" name="phoneNumberFile" placeholder="phone number" 
+          value={phoneNumberFile} 
+          onChange={e => setPhoneNumberFile(e.target.value)}/>
+
+          <input type="text" name="fileUrl" placeholder="file URL" 
+          value={fileUrl} 
+          onChange={e => setFileUrl(e.target.value)}/>
+
+          <button type="button" onClick={handleSendFile}>sendFileByUrl</button>
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="responseField">
+        <label htmlFor="response">Ответ:</label>
+        <textarea id="response" readOnly={true} name="response" 
+        value={responseData} rows="30" cols="50"></textarea>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+    </div>
     </>
   )
 }
